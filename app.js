52a3e90737d8aeebacd2b7a418a0b28ed2fdf0b2
Neha Gupta <32819951+nehagupta1804@users.noreply.github.com>
2019-06-22 16:52:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,11 +28,20 @@ server.listen(process.env.PORT, function () {
 server.post('/messages', (req, res) =>
 	    
 	    
-	    {	    const sessionId = uuid.v4();
+	    {	
+	var config = {
+  credentials: {
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQClVL3GqvpS/mXC\n7Uh1uAE06GXNP/jhBqOiiiVidGtC47WMYDb0RjX+FArdinOr2rSeKqgvwx5uRQjP\n6YbLfhRu/grPGXXN9HiDkc9Rw3YPM2qzklFbedtGTXkac9gMtfoW/2HdbiANG1mO\nms2s3IDx+zMDqPCwXxAO7z2XOCOylC0qQoV9Qyjeo2BUjunQc0SioZwprNItaZUF\noT5Ilae/ad3kbF55J0fqDt6bFNlKUgtt+VmTHiW0oQXINOjLx7Aeo74UkOWwimxW\nQ43BqMSJhMiwVf/H51iCGHBfjoD2hUl/AUCIhw6+F0Zeg+4MzDnRipl8edZNp1KI\nK9SlwKuHAgMBAAECggEAGZZjZp9omDandi3Lae39QHx6i55hJ3vOZdCpQjMQCgKm\n7YpTMaa6ItAO8cRElwOFXPiaHgY91prC5zartiEVtYCGGLub2AZZg8ypwEGW1c8Y\ng6Ewto5wntFNCF3ca/SFYlDKYH7zi+5rLl8MAjsAOTMuQoc/dPR0I549iIKs7QKy\nXQVbIMkAKZ9EmYtLMYC6g1fpZrMAGELoummE6pkde/gCOwj1NC/Pwr/mYi4CHJSm\nhcV2HgAfvITTKzwRbezJFtdN3cQDw4LSdpCcVlofjShmGzlnkVB3wGzHah7C4MrR\nQ7PLW0hO36cOJ5zOtZcTjJcRT4fhyZAUV+RdeFTzqQKBgQDZP7+5xOeZxCLUY0vU\nTJ/Q8tI/TKCw7N1lr1TBiYecQ5fnf5ZBFxA0BxERzF3x9m221Y/6EmyXASDb2osC\nvHEavp8SuSScR5GS/hk5mx80DX7uSk2VHIJliSrmMANLeqQPvBfPfahAIvQa3jfE\n0mtczhBapxOb+bn15askusjHiQKBgQDC0kI2aqNGLeeW9talHJ2AiCb5uWDHqpZH\n7djoR8n+Liq1OoapyW29IxIHw9J4bWTeTuf8T/zmVmc48K6l4VivueLbF3bmM4va\nVwpSw2fxkLdfZjFEkbVCb/tYe7h2oC17K0WBXteZ+FPdsSUyHWvkyHxUiGn8FZ0+\n4vRznIwGjwKBgApPMiP4qhzr59M4WxLaCAN8YmnKkx2SP/dhWJ5Ojnbl5kGAFntE\nQuqVI0Q69IJU9WPlwo1Vbn3D+fE077/TfXKNdTMIQiqAYvIcyMmpCNQJh+p68C+R\nSMbUuYpu4NkmGr78aAzao69XeUoFHZRjf+DJjpEeuJmAQkje+z4k9v4xAoGARilW\nsyZAWtvZVaLbtmbRyiI+dxzt37ptmMXtoemIs0OJGe9T4/2oyb7BlY5OVBSBiRY9\nsb5Uw9dGIVl8bDz1pYN6UZDnvwu9SAqkqx24Z2TIxVCWWa1n699MU5dq3T3eY0c4\nJtkzKD4oZeSQErZJzwDs1byy/pgCI02035X3T70CgYEAn7WB+HYkGPYmCBJ6a8UP\n9XxU7FsqEcADBlm0BiZgkVYovRPJCNJKvvJZkpZBKuXfWS7HIVDLpYvfLkG2PmuM\n6asphwM5j5CkFKpImO/u7iTvvsodTo1m/L8OrMHLiVJW4rSa36E0RQTWPGVktTTf\nrYOBb84QmJOdBiSnhk9XImk=\n-----END PRIVATE KEY-----",
+    client_email: "stockprice-bibxgw@appspot.gserviceaccount.com"
+  }
+}
+
+const sessionClient = new dialogflow.SessionsClient(config);
+	const sessionId = uuid.v4();
         const  projectId='stockprice-bibxgw';
 
   // Create a new session
-  const sessionClient = new dialogflow.SessionsClient("63aeaada0df0743bd7b64d395a61f36a8746b54d","stockprice-bibxgw@appspot.gserviceaccount.com");
+  const sessionClient = new dialogflow.SessionsClient(config);
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
   // The text query request.
