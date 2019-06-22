5ea6716bbfb5a149008cf91556878d891f65523f
Neha Gupta <32819951+nehagupta1804@users.noreply.github.com>
2019-06-22 15:35:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Smooch = require('smooch-core');
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+@param {string} projectId 
 
 
 const KEY_ID = 'app_5d0bc7845cd7540010b82fc9';
@@ -23,9 +26,42 @@ server.listen(process.env.PORT, function () {
 
 
 server.post('/messages', (req, res) =>
+	    
+	    
+	const sessionId = uuid.v4();
+        const  projectId='stockprice-bibxgw';
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: 'hello',
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
 	
 	
-	{console.log('reached request');     
+   
 		 smooch.appUsers
     .sendMessage({
         userId: 'e83c4e435a64724dcb18286a',
@@ -43,7 +79,7 @@ server.post('/messages', (req, res) =>
         console.log('API ERROR:\n', err);
         res.end();
     });
-	}
+	
 	
 
 	   
